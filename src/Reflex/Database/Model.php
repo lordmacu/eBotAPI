443939fcd75008393937449af84ae666bd2e0919
Uniquoooo <cheese5505@gmail.com>
2015-09-27 16:45:02 +1300

    Allowing different connections to be used

--- src/Reflex/Database/Model.php
+++ src/Reflex/Database/Model.php
@@ -5,4 +5,13 @@ namespace Reflex\Database;
 use Illuminate\Database\Eloquent\Model as BaseModel;
 
 class Model extends BaseModel
-{}
+{
+	protected $connection;
+
+	public function __construct(array $attributes = [])
+	{
+		$this->connection = config('ebot-reflex.connection');
+
+		parent::__construct($attributes);
+	}
+}
--- src/config/config.php
+++ src/config/config.php
@@ -2,8 +2,10 @@
 
 return [
 	
-	'ebot_ip'	=> env('EBOT_IP'), // The IP used for connecting to the eBot websocket
+	'ebot_ip'		=> env('EBOT_IP'), // The IP used for connecting to the eBot websocket
 
-	'ebot_port'	=> env('EBOT_PORT', 12360) // The port used for connecting to the eBot websocket
+	'ebot_port'		=> env('EBOT_PORT', 12360), // The port used for connecting to the eBot websocket
+
+	'connection'	=> config('database.default')
 
 ];
