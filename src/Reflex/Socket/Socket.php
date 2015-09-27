90b509ddf67bf2abf6abaf5aabe8ab082658d9d8
Uniquoooo <cheese5505@gmail.com>
2015-09-27 15:55:06 +1300

    [Laravel] Laravel!

--- src/Reflex/Socket/Socket.php
+++ src/Reflex/Socket/Socket.php
@@ -8,13 +8,13 @@ class Socket
 {
 	public static function send($command, $match, $extra = '')
 	{
-		$config = eBotIP::getInstance();
+		$config = config('ebot-reflex');
 		$data = "{$match->id} {$command} {$match->server->ip}";
 		if(!empty($extra)) $data = "{$data} {$extra}";
 		$data = Encryption::encrypt($data, $match->config_authkey, 256);
 		$data = json_encode([$data, $match->server->ip]);
 
 		$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-		return socket_sendto($socket, $data, strlen($data), 0, $config->geteBotIP(), $config->geteBotPort());
+		return socket_sendto($socket, $data, strlen($data), 0, $config['ebot_ip'], $config['ebot_port']);
 	}
 }
